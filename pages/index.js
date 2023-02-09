27c84b6a2ffe25032dbb151f8358a28b3dda72d8
PragyaT26 <pragyatiwari26052001@gmail.com>
2023-02-10 01:33:55 +0530

    resume added

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
+                  href="https://drive.google.com/file/d/12zsadclIWAqI2m2Q9XZ3PmVeZ6n-Mvfw/view?usp=share_link" target="_blank"
                 >
                   Resume
                 </a>
